20292cba1a7be50e8542b08d2b1249c7b62930dd
Kemal Tarık Nehrozoğlu <tarikn2003@gmail.com>
2024-03-29 16:44:19 +0300

    tests for compute

--- src/test/java/TestCompute.java
+++ src/test/java/TestCompute.java
@@ -14,4 +14,29 @@ public class TestCompute {
     c = new Compute(mq);
     assertTrue(true);
   }
+  @Test
+  public void emptyQueue() {
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+    when(mq.size()).thenReturn(0);
+    assertEquals(-1,c.countNumberOfOccurrences("hello"));
+    
+  }
+  @Test
+  public void NoOccurence() {
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+    when(mq.contains("hello")).thenReturn(false);
+    assertEquals(0,c.countNumberOfOccurrences("hello"));
+  }
+  @Test
+  public void YesOccurance() {
+    MessageQueue mq = mock(MessageQueue.class);
+    c = new Compute(mq);
+    when(mq.size()).thenReturn(3);
+    when(mq.getAt(0)).thenReturn("hello");
+    when(mq.getAt(1)).thenReturn("Nope");
+    when(mq.getAt(2)).thenReturn("hello");
+    assertEquals(3,c.countNumberOfOccurrences("hello"));
+  }
 }

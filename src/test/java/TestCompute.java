10c5facb0c090c1103d6ad237fb5145b3604e136
Kemal Tarık Nehrozoğlu <tarikn2003@gmail.com>
2024-03-29 17:24:07 +0300

    changes in testcompute

--- src/test/java/TestCompute.java
+++ src/test/java/TestCompute.java
@@ -8,12 +8,7 @@ import org.junit.Test;
 public class TestCompute {
   Compute c;
 
-  @Test
-  public void example() {
-    MessageQueue mq = mock(MessageQueue.class);
-    c = new Compute(mq);
-    assertTrue(true);
-  }
+  
   @Test
   public void emptyQueue() {
     MessageQueue mq = mock(MessageQueue.class);
@@ -26,6 +21,7 @@ public class TestCompute {
   public void NoOccurence() {
     MessageQueue mq = mock(MessageQueue.class);
     c = new Compute(mq);
+    when(mq.size()).thenReturn(1);
     when(mq.contains("hello")).thenReturn(false);
     assertEquals(0,c.countNumberOfOccurrences("hello"));
   }
@@ -34,9 +30,10 @@ public class TestCompute {
     MessageQueue mq = mock(MessageQueue.class);
     c = new Compute(mq);
     when(mq.size()).thenReturn(3);
+    when(mq.contains("hello")).thenReturn(true);
     when(mq.getAt(0)).thenReturn("hello");
     when(mq.getAt(1)).thenReturn("Nope");
     when(mq.getAt(2)).thenReturn("hello");
-    assertEquals(3,c.countNumberOfOccurrences("hello"));
+    assertEquals(2,c.countNumberOfOccurrences("hello"));
   }
 }
